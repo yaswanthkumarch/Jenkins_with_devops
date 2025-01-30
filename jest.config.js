e55cbb112e60744fb73b17cf0d553e66a02b462e
MTL1020 <vamsimohanyacham@gmail.com>
2025-01-30 15:05:24 +0530

    hiibro

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,12 @@ module.exports = {
     "**/?(*.)+(spec|test).[tj]s?(x)", // Matches test files
   ],
   testPathIgnorePatterns: ["/node_modules/"], // Ignores node_modules during tests
+
+  reporters: [
+    'default', // Default reporter
+    ['jest-junit', {
+      outputDirectory: 'test-results', // Output directory for JUnit report
+      outputName: 'test-results.xml'   // Output filename for JUnit report (optional)
+    }]
+  ],
 };
