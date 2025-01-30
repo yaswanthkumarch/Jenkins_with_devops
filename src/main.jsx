5c8c9d4608090915508cbcfc87cbba298cbed44d
vamsimohanyacham <142957313+vamsimohanyacham@users.noreply.github.com>
2025-01-30 12:37:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-#import { StrictMode } from 'react'
+// import { StrictMode } from 'react'; // This line can be removed
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
