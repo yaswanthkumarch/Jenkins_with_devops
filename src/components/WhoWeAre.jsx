484172263460231ba51e317ba3573e9a71a1228e
vamsimohanyacham <142957313+vamsimohanyacham@users.noreply.github.com>
2025-01-30 12:44:12 +0530

    Update WhoWeAre.jsx

--- src/components/WhoWeAre.jsx
+++ src/components/WhoWeAre.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react'; //
 
 const NumberTicker = ({ end, duration }) => {
     const [count, setCount] = useState(0);
