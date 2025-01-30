32f427b4d243629dcc83f2493fcfde9adaac6ce7
MTL1020 <vamsimohanyacham@gmail.com>
2025-01-30 14:33:44 +0530

    hi vamsi

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,36 @@
-import globals from 'globals'; // Import globals package
+import { defineConfig } from 'eslint-define-config';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
 
-export default [
-  {
-    files: ['**/*.js', '**/*.jsx'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: {
-        ...globals.browser, // Spread the browser globals
-        test: 'readonly',  // Add Jest globals here
-        expect: 'readonly', // Add Jest globals here
-        module: 'readonly'  // Add Jest globals here
-      },
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+export default defineConfig({
+  files: ['**/*.js', '**/*.jsx'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: {
+      ...globals.browser,
+      test: 'readonly',  // Jest globals
+      expect: 'readonly', // Jest globals
+      module: 'readonly', // Jest globals
     },
-    settings: {
-      react: {
-        version: '18.3' // Specify React version
-      },
-    },
-    plugins: [
-      react,
-      'react-hooks',
-      'react-refresh',
-    ],
-    rules: {
-      // Your custom ESLint rules can be added here, or you can spread rules from plugins
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off', // Example rule customization
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      ecmaFeatures: { jsx: true },
+      sourceType: 'module',
     },
   },
-];
+  settings: {
+    react: {
+      version: '18.3',
+    },
+  },
+  plugins: {
+    react,
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    // ESLint rules here
+  },
+});
